a6ea2ccda48d1287aabb9af05f00ba1f6f18c8e1
daymonjohnson <38265232+daymonjohnson@users.noreply.github.com>
2018-05-22 00:05:28 -0400

    Add files via upload

--- todoApp.js
+++ todoApp.js
@@ -2,40 +2,39 @@
 {
   function list() {
     let vm = this;
-    vm.chores = ["Clean", "Read", "Pack", "Laundry"]
-    
-};
+    vm.chores = ["Clean", "Read", "Pack", "Laundry",]
+    vm.add = function(chore){
+      console.log(vm.chores);
+      let y = {chore};
+    vm.chores.push(chore);
+    console.log(vm.chores);
+    };
+    vm.removeTask = function(){
+      console.log(vm.chores);
+    vm.chores.splice($index ,1);
+    };
 
-var fetch = angular.module('todoapp', []);
+ }
+
+ var fetch = angular.module('todoapp', []);
 
    fetch.controller('complete', ['$scope', '$http', function ($scope, $http) {
      $scope.completeClass = false;
      // Add red remove
      $scope.taskCompleted = function(){
          console.log("yassssssss");
-       $scope.completeClass = true;
-
+     $scope.completeClass = true;
      }
-   }]);
 
-   fetch.controller('remove', ['$scope', '$http', function ($scope, $http) {
-   // $scope.completeClass = false;
-    //  remove task
-     $scope.removeTask = function(){
-         console.log("pawg");
-         $scope.removeTask = true;
-    //     console.log("yassssssss");
-    //   $scope.completeClass = true;
-
-    }
-  }]); 
-
-   
-
+    // $scope.removeTask = false;
+    //  $scope.removeTask = function(){
+    //     console.log("vm.chores.splice(2)");
+    //   chores.splice($index,1);
+    //  $scope.removeTask = true;
+    //  }
+   }   
+  ]); 
   angular
     .module("todoapp")
     .controller("list", list);
 };
-
-
-
